409f7a9de8f4e36d7f76a8f830dd006e6cc18693
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-03-17 12:10:44 +0100

    Corrections in the Java generator

--- processor-java/src/main/java/testaccessors/internal/AccessorWriter.java
+++ processor-java/src/main/java/testaccessors/internal/AccessorWriter.java
@@ -10,6 +10,8 @@ import javax.lang.model.element.Modifier;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 final class AccessorWriter extends AbstractAccessorWriter {
     private final Logger logger;
@@ -20,24 +22,29 @@ final class AccessorWriter extends AbstractAccessorWriter {
 
     @Override
     public void writeAccessorClass(final Set<Element> annotatedElements, final Filer filer) {
-        final ClassName className = (ClassName) ClassName.get(annotatedElements.iterator().next().getEnclosingElement().asType());
-        final TypeSpec.Builder classSpecBuilder = TypeSpec.classBuilder(nameForGeneratedClassFrom(className.simpleNames()))
+        final ClassName enclosingType = (ClassName) TypeName.get(annotatedElements.iterator().next().getEnclosingElement().asType());
+        final TypeSpec.Builder classSpecBuilder = TypeSpec.classBuilder(nameForGeneratedClassFrom(enclosingType.simpleNames()))
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
         annotatedElements.stream()
-                .map(new java.util.function.Function<Element, MethodSpec[]>() {
+                .flatMap(new Function<Element, Stream<MethodSpec>>() {
                     @Override
-                    public MethodSpec[] apply(final Element element) {
+                    public Stream<MethodSpec> apply(final Element element) {
                         final RequiresAccessor.AccessorType[] requires = element.getAnnotation(
                                 RequiresAccessor.class).requires();
-                        final MethodSpec[] methodSpec = new MethodSpec[requires.length];
+                        final MethodSpec[] methodSpecs = new MethodSpec[requires.length];
                         for (int i = 0; i < requires.length; i++) {
-                            if (requires[i] == RequiresAccessor.AccessorType.TYPE_GETTER) {
-                                methodSpec[i] = generateGetterMethodSpec(element);
-                            } else {
-                                methodSpec[i] = generateSetterMethodSpec(element);
+                            switch (requires[i]) {
+                                case TYPE_GETTER:
+                                    methodSpecs[i] = generateGetterMethodSpec(element);
+                                    break;
+                                case TYPE_SETTER:
+                                    methodSpecs[i] = generateSetterMethodSpec(element);
+                                    break;
+                                default:
+                                    throw illegalAccessorRequestedException(element);
                             }
                         }
-                        return methodSpec;
+                        return Arrays.stream(methodSpecs);
                     }
 
                     private MethodSpec generateGetterMethodSpec(final Element element) {
@@ -45,37 +52,38 @@ final class AccessorWriter extends AbstractAccessorWriter {
                         return MethodSpec.methodBuilder(annotation.name().isEmpty() ? element.getSimpleName().toString() : annotation.name())
                                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                                 .addParameter(ParameterSpec.builder(
-                                        className,
+                                        TypeName.get(element.asType()),
                                         PARAMETER_NAME_RECEIVER,
                                         Modifier.FINAL)
                                         .build())
-                                .addStatement("return " + element.getSimpleName())
-                                .returns(ClassName.get(element.asType()))
+                                .addStatement("return " + PARAMETER_NAME_RECEIVER + "." + element.getSimpleName())
+                                .returns(TypeName.get(element.asType()))
                                 .build();
                     }
 
                     private MethodSpec generateSetterMethodSpec(final Element element) {
+                        final TypeName fieldType = TypeName.get(element.asType());
                         final RequiresAccessor annotation = element.getAnnotation(RequiresAccessor.class);
                         return MethodSpec.methodBuilder(annotation.name().isEmpty() ? element.getSimpleName().toString() : annotation.name())
                                 .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
                                 .addParameter(ParameterSpec.builder(
-                                        className,
+                                        fieldType,
                                         PARAMETER_NAME_RECEIVER,
                                         Modifier.FINAL)
                                         .build())
                                 .addParameter(ParameterSpec.builder(
-                                        ClassName.get(element.asType()),
-                                        setterParameterName(),
+                                        fieldType,
+                                        PARAMETER_NAME_NEW_VALUE,
                                         Modifier.FINAL)
                                         .build())
+                                .addStatement(PARAMETER_NAME_RECEIVER + "." + element.getSimpleName() + " = " + PARAMETER_NAME_NEW_VALUE)
                                 .returns(void.class)
                                 .build();
                     }
                 })
-                .flatMap(Arrays::stream)
                 .forEach(classSpecBuilder::addMethod);
         try {
-            JavaFile.builder(className.packageName(), classSpecBuilder.build()).build().writeTo(filer);
+            JavaFile.builder(enclosingType.packageName(), classSpecBuilder.build()).build().writeTo(filer);
         } catch (final IOException e) {
             logger.error(Arrays.toString(e.getStackTrace()));
         }
--- processors-interfaces/src/main/java/testaccessors/internal/AbstractAccessorWriter.java
+++ processors-interfaces/src/main/java/testaccessors/internal/AbstractAccessorWriter.java
@@ -10,9 +10,11 @@ abstract class AbstractAccessorWriter {
         return String.join("$", enclosingClassSimpleNames) + "TestAccessors";
     }
 
-    final String setterParameterName() {
-        return "newValue";
+    final RuntimeException illegalAccessorRequestedException(final Element element) {
+        return new IllegalStateException("Illegal accessor type requested " + element.getSimpleName());
     }
 
     abstract void writeAccessorClass(final Set<Element> annotatedElements, final Filer filer);
+
+    static final String PARAMETER_NAME_NEW_VALUE = "newValue";
 }
