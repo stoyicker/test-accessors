4f42dc5236b46d0689a01cd49610a1008fcd1153
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-03-24 18:19:09 +0100

    Better sample

--- sample/src/main/kotlin/sample/banana/kotlin/KotlinClass.kt
+++ sample/src/main/kotlin/sample/banana/kotlin/KotlinClass.kt
@@ -2,22 +2,29 @@ package sample.banana.kotlin
 
 import testaccessors.RequiresAccessor
 
-class RootClass<A, B, C, D, E, F, G> {
+class First<A, B, C, D, E, F, G> {
 	@RequiresAccessor
 	private val aField: String? = null
 	private val anotherTopLevelField: String? = null
 
-	class AbcClass {
-		class StaticMiddleClass {
-			class StaticInnerClass<T, J : Set<List<T>>, Q> {
+	class Second {
+		inner class Third<A> {
+			@RequiresAccessor(requires = [RequiresAccessor.AccessorType.TYPE_SETTER])
+			private val yetAnotherField = Unit
+			inner class Fourth {
+				@RequiresAccessor(requires = [RequiresAccessor.AccessorType.TYPE_SETTER])
+				private val yetAnotherField = Unit
+				inner class Fifth<A> {
+					@RequiresAccessor(requires = [RequiresAccessor.AccessorType.TYPE_SETTER])
+					private val yetAnotherField = Unit
+				}
+			}
+		}
+		class Sixth {
+			class Seventh<T, J : Set<List<T>>, Q> {
 				@RequiresAccessor(name = "middleFieldThatHasBeenRenamed")
 				private val anotherField = emptySet<Q>()
 			}
 		}
-	}
-
-	inner class InnerClass<A> {
-		@RequiresAccessor(requires = [RequiresAccessor.AccessorType.TYPE_SETTER])
-		private val yetAnotherField = Unit
 	}
 }
