8a16c3c132d492c54680615964bee384ac7860ea
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-03-24 10:11:27 +0100

    Leave KotlinClass in sample uncommented

--- sample/src/main/kotlin/sample/kotlin/KotlinClass.kt
+++ sample/src/main/kotlin/sample/kotlin/KotlinClass.kt
@@ -1,16 +1,23 @@
 package sample.kotlin
 
-//import testaccessors.RequiresAccessor
+import testaccessors.RequiresAccessor
 
-//class KotlinClass<A, B, C, D, E, F, G> {
-//	@RequiresAccessor
-//	private val aField: String? = null
-//	class StaticInnerClass<T, J : Set<List<T>>, Q> {
-//		@RequiresAccessor(name = "middleField")
-//		private val anotherField = emptySet<Q>()
-//		inner class InnerClass<A> {
-//			@RequiresAccessor
-//			private val yetAnotherField = Unit
-//		}
-//	}
-//}
+class KotlinClass<A, B, C, D, E, F, G> {
+	@RequiresAccessor
+	private val aField: String? = null
+	private val anotherTopLevelField: String? = null
+
+	class AbcClass {
+		class StaticMiddleClass {
+			class StaticInnerClass<T, J : Set<List<T>>, Q> {
+				@RequiresAccessor(name = "middleFieldThatHasBeenRenamed")
+				private val anotherField = emptySet<Q>()
+			}
+		}
+	}
+
+	inner class InnerClass<A> {
+		@RequiresAccessor(requires = [RequiresAccessor.AccessorType.TYPE_SETTER])
+		private val yetAnotherField = Unit
+	}
+}
