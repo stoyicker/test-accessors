f8fe98cc3cbe3373af09eb52878f246b79c702e9
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-03-24 10:11:48 +0100

    Some fixes in the Kotlin artifact

--- processor-kotlin/src/main/kotlin/testaccessors/internal/AccessorWriter.kt
+++ processor-kotlin/src/main/kotlin/testaccessors/internal/AccessorWriter.kt
@@ -12,19 +12,23 @@ import testaccessors.RequiresAccessor
 import javax.annotation.Generated
 import javax.annotation.processing.Filer
 import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.Modifier
 import javax.lang.model.type.DeclaredType
-import javax.lang.model.type.TypeVariable
 import javax.lang.model.util.Elements
 import javax.lang.model.util.Types
 import javax.tools.StandardLocation
 
 internal class AccessorWriter(types: Types, elementUtils: Elements) : AbstractAccessorWriter(types, elementUtils) {
 	public override fun writeAccessorClass(annotatedElements: Set<Element>, filer: Filer) {
-		// TODO Correctly place/name inner classes and static inner classes
 		val enclosingClassElement = annotatedElements.iterator().next().enclosingElement
 		val enclosingClassPackage = elementUtils.getPackageOf(enclosingClassElement)
+		val classNames = extractParentClasses(enclosingClassElement.enclosingElement).reversedArray() +
+				enclosingClassElement.simpleName.toString()
 		val enclosingClassErased = ClassName(
-				enclosingClassPackage.qualifiedName.toString(), enclosingClassElement.simpleName.toString())
+				enclosingClassPackage.qualifiedName.toString(),
+				classNames.first(),
+				*classNames.sliceArray(1..classNames.lastIndex))
 		val classAndFileName = nameForGeneratedClassFrom(enclosingClassErased.simpleNames)
 		val typeSpecBuilder = TypeSpec.objectBuilder(classAndFileName)
 				.addAnnotation(Generated::class)
@@ -32,44 +36,43 @@ internal class AccessorWriter(types: Types, elementUtils: Elements) : AbstractAc
 			override fun invoke(element: Element) =
 					element.getAnnotation(RequiresAccessor::class.java).requires.map {
 						when (it) {
-							RequiresAccessor.AccessorType.TYPE_GETTER -> generateGetterMethodSpec(element)
-							RequiresAccessor.AccessorType.TYPE_SETTER -> generateSetterMethodSpec(element)
+							RequiresAccessor.AccessorType.TYPE_GETTER -> generateGetterFunSpec(element)
+							RequiresAccessor.AccessorType.TYPE_SETTER -> generateSetterFunSpec(element)
 							else -> throw illegalAccessorRequestedException(element)
 						}
 					}
 
-			private fun generateGetterMethodSpec(element: Element) = element.getAnnotation(RequiresAccessor::class.java)
-					.run {
-						FunSpec.builder(if (name.isEmpty()) element.simpleName.toString() else name)
-								.addAnnotation(JvmStatic::class)
-								.addTypeVariables((enclosingClassElement.asType() as DeclaredType).typeArguments.map {
-									(it as TypeVariable).let {
-										TypeVariableName.invoke(it.asElement().simpleName.toString()).apply {
-										}
-									}
-								})
-								.receiver(enclosingClassElement.asType().asTypeName())
-								.addStatement("return " + element.simpleName)
-								.build()
-					}
+			private fun generateGetterFunSpec(element: Element) = generateCommonFunSpec(element)
+					.addStatement("return " + element.simpleName)
+					.build()
 
-			private fun generateSetterMethodSpec(element: Element) = element.getAnnotation(RequiresAccessor::class.java)
+			private fun generateSetterFunSpec(element: Element) = generateCommonFunSpec(element)
+					.addParameter(ParameterSpec.builder(
+							PARAMETER_NAME_NEW_VALUE,
+							element.asType().asTypeName())
+							.build())
+					.addStatement("${element.simpleName} = $PARAMETER_NAME_NEW_VALUE")
+					.build()
+
+			private fun generateCommonFunSpec(element: Element) = element.getAnnotation(RequiresAccessor::class.java)
 					.run {
+						val typeVariables = mutableSetOf<TypeVariableName>()
+						typeVariables += (enclosingClassElement.asType() as DeclaredType).typeArguments.map {
+							TypeVariableName(it.asTypeName().toString())
+						}
+						var enclosingElement = enclosingClassElement.enclosingElement
+						while (enclosingElement != null &&
+								!enclosingElement.modifiers.contains(Modifier.STATIC) &&
+								enclosingElement.kind != ElementKind.PACKAGE) {
+							typeVariables += (enclosingElement.asType() as DeclaredType).typeArguments.map {
+								TypeVariableName(it.asTypeName().toString())
+							}
+							enclosingElement = enclosingElement.enclosingElement
+						}
 						FunSpec.builder(if (name.isEmpty()) element.simpleName.toString() else name)
 								.addAnnotation(JvmStatic::class)
-								.addTypeVariables((enclosingClassElement.asType() as DeclaredType).typeArguments.map {
-									(it as TypeVariable).let {
-										TypeVariableName.invoke(it.asElement().simpleName.toString()).apply {
-										}
-									}
-								})
+								.addTypeVariables(typeVariables)
 								.receiver(enclosingClassElement.asType().asTypeName())
-								.addParameter(ParameterSpec.builder(
-										PARAMETER_NAME_NEW_VALUE,
-										element.asType().asTypeName())
-										.build())
-								.addStatement("${element.simpleName} = $PARAMETER_NAME_NEW_VALUE")
-								.build()
 					}
 		}).forEach { typeSpecBuilder.addFunction(it) }
 		FileSpec.builder(enclosingClassPackage.simpleName.toString(), classAndFileName)
@@ -82,6 +85,20 @@ internal class AccessorWriter(types: Types, elementUtils: Elements) : AbstractAc
 				.build()
 				.writeTo(filer)
 	}
+
+	private fun isTopLevel(element: Element?): Boolean = element?.run {
+		when (enclosingElement) {
+			null -> true
+			else -> enclosingElement.kind == ElementKind.PACKAGE
+		}
+	} ?: true
+
+	private fun extractParentClasses(element: Element?): Array<String> = element?.let {
+		arrayOf(it.simpleName.toString()) + when (isTopLevel(it.enclosingElement)) {
+			true -> if (it.enclosingElement == null) emptyArray() else arrayOf(it.enclosingElement.simpleName.toString())
+			false -> extractParentClasses(it.enclosingElement)
+		}
+	} ?: emptyArray()
 
 	// FIXME KotlinPoet will hopefully add support for Filer at some point in the future
 	private fun FileSpec.writeTo(filer: Filer) {
