bfcc6b5076e23a4e2edb4a358e6d942f1f1e32d8
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-04-24 17:42:30 +0200

    Fix #20

---
+++ sample-java/src/test/java/sample/banana/java/FifthTest.java
@@ -0,0 +1,49 @@
+package sample.banana.java;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.mock;
+
+final class FifthTest {
+  private First.Second.Third.Fourth.Fifth subject;
+
+  @BeforeEach
+  void setUp() {
+    subject = new First.Second().new Third<>().new Fourth().new Fifth<String>();
+  }
+
+  @Test
+  void setAField() throws NoSuchFieldException, IllegalAccessException {
+    //noinspection unchecked
+    final Set<String> expected = mock(Set.class);
+
+    FirstSecondThirdFourthFifthTestAccessors.yetAnotherField(subject, expected);
+
+    final Field field = subject.getClass().getDeclaredField("yetAnotherField");
+    final boolean wasAccessible = field.isAccessible();
+    field.setAccessible(true);
+    final Set<String> actual = (Set<String>) field.get(subject);
+    assertSame(expected, actual);
+    field.setAccessible(wasAccessible);
+  }
+
+  @Test
+  void getAField() throws NoSuchFieldException, IllegalAccessException {
+    //noinspection unchecked
+    final Set<String> expected = mock(Set.class);
+    final Field field = subject.getClass().getDeclaredField("yetAnotherField");
+    final boolean wasAccessible = field.isAccessible();
+    field.setAccessible(true);
+    field.set(subject, expected);
+    field.setAccessible(wasAccessible);
+
+    final Set<String> actual = FirstSecondThirdFourthFifthTestAccessors.yetAnotherField(subject);
+
+    assertSame(expected, actual);
+  }
+}
---
+++ sample-java/src/test/java/sample/banana/java/ThirdTest.java
@@ -0,0 +1,31 @@
+package sample.banana.java;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+final class ThirdTest {
+  private First.Second.Third<String> subject;
+
+  @BeforeEach
+  void setUp() {
+    subject = new First.Second().new Third<>();
+  }
+
+  @Test
+  void setAField() throws NoSuchFieldException, IllegalAccessException {
+    final String expected = "this is a mock value";
+
+    FirstSecondThirdTestAccessors.yetAnotherField(subject, expected);
+
+    final Field field = subject.getClass().getDeclaredField("yetAnotherField");
+    final boolean wasAccessible = field.isAccessible();
+    field.setAccessible(true);
+    final String actual = (String) field.get(subject);
+    assertSame(expected, actual);
+    field.setAccessible(wasAccessible);
+  }
+}
