ddbcfb5945dccef79489a3cc45b791e8efb620b6
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-04-30 18:05:23 +0200

    No release notes

--- .circleci/release_github.sh
+++ .circleci/release_github.sh
@@ -8,15 +8,6 @@ json_escape() {
 uploadReleaseToGitHub() {
     git fetch --tags
     THIS_TAG=$(git describe --tags --abbrev=0)
-    PREVIOUS_TAG=$(git describe --tags --abbrev=0 ${THIS_TAG}^)
-    OLD_IFS=$IFS
-    local IFS=$'\n'
-    RELEASE_NOTES_ARRAY=($(git log --format=%B ${PREVIOUS_TAG}..${THIS_TAG} | tr -d '\r'))
-    { for i in "${RELEASE_NOTES_ARRAY[@]}"
-    do
-        RELEASE_NOTES="$RELEASE_NOTES$i\n"
-    done
-    }
 
     BODY="{
         \"tag_name\": \"$THIS_TAG\",
@@ -65,29 +56,6 @@ uploadReleaseToGitHub() {
         --data-binary "@processor-java.jar" \
         --request POST \
         ${PROCESSOR_JAVA_UPLOAD_URL}
-
-    local IFS=$OLD_IFS
-    RELEASE_NOTES_BODY="\n**CHANGELOG**:\n$RELEASE_NOTES"
-
-    echo -e $RELEASE_NOTES_BODY > changelog.txt
-
-    json_escape $(cat changelog.txt) > changelog.txt
-
-    BODY="{
-        \"body\": $(cat changelog.txt)
-    }"
-
-    echo $BODY > changelog.json
-
-    # Attach the release notes
-    curl -s \
-        -v \
-        -u ${CIRCLE_USERNAME}:${GITHUB_TOKEN} \
-        --header "Accept: application/vnd.github.v3+json" \
-        --header "Content-Type: application/json; charset=utf-8" \
-        --request PATCH \
-        --data @changelog.json \
-        https://api.github.com/repos/"${REPO_SLUG}"/releases/${RELEASE_ID}
 
     echo "GitHub release complete."
 }
