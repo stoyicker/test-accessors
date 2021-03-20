98b01196738d857871910fcde7809c4d799a6a97
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2021-03-20 13:30:30 +0100

    Fix #120

--- processor-base/src/main/kotlin/testaccessors/internal/base/RequiresAccessorAnnotationVerifier.kt
+++ processor-base/src/main/kotlin/testaccessors/internal/base/RequiresAccessorAnnotationVerifier.kt
@@ -1,7 +1,9 @@
 package testaccessors.internal.base
 
 import testaccessors.RequiresAccessor
+import java.util.*
 import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
 import javax.lang.model.element.Modifier
 
 class RequiresAccessorAnnotationVerifier(logger: Logger)
@@ -10,12 +12,27 @@ class RequiresAccessorAnnotationVerifier(logger: Logger)
   override fun annotationClass() = RequiresAccessor::class.java
 
   override fun verify(element: Element, annotation: RequiresAccessor) =
-      element.modifiers.run {
-        if (contains(Modifier.PUBLIC) && !contains(Modifier.FINAL)) {
-          logger.warn("Fields that are public and non-final are guaranteed to be accessible from anywhere at anytime, so you don't want to generate accessors for them.", element)
-          false
-        } else {
-          true
+      element.run {
+        when {
+          isPublicAndFinal() -> {
+            logger.error("Fields that are public and non-final are guaranteed to be freely manipulated from anywhere, so you don't need to generate accessors for them.", element)
+            false
+          }
+          isPropertyWithoutBackingField() -> {
+            logger.error("Properties without backing fields are not supported.", element)
+            false
+          }
+          else -> {
+            true
+          }
         }
       }
+
+  private fun Element.isPublicAndFinal() = modifiers.run { contains(Modifier.PUBLIC) && !contains(Modifier.FINAL) }
+
+  private fun Element.isPropertyWithoutBackingField() = run {
+    kind != ElementKind.FIELD && enclosingElement.enclosedElements.any {
+      it.simpleName.toString().toLowerCase(Locale.ENGLISH).contentEquals(this.simpleName.substring(3).toLowerCase(Locale.ENGLISH))
+    }
+  }
 }
