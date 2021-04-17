1febfe8b9a1bde59f451ac615de14c966dd9b1f9
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2021-04-17 08:56:02 +0200

    Remove processor-kotlin

--- sample-android/src/kotlin/java/root/MyObject.kt
+++
@@ -1,8 +0,0 @@
-package root
-
-import testaccessors.RequiresAccessor
-
-object MyObject {
-  @RequiresAccessor(requires = [RequiresAccessor.AccessorType.TYPE_GETTER, RequiresAccessor.AccessorType.TYPE_SETTER])
-  private const val myField = "holahola"
-}
