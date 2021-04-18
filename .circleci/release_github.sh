86a5e822d7f1bad3b7c644b954639309882b6e2f
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2021-04-18 13:49:38 +0200

    Debug

--- .circleci/release_github.sh
+++ .circleci/release_github.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -e
+set -xe
 
 uploadReleaseToGitHub() {
     git fetch --tags
     THIS_TAG=$(git describe --tags --abbrev=0)
+
+    echo "This tag: $THIS_TAG"
 
     BODY="{
         \"tag_name\": \"$THIS_TAG\",
@@ -11,6 +13,8 @@ uploadReleaseToGitHub() {
         \"name\": \"$THIS_TAG\",
         \"body\": \" \"
     }"
+
+    echo "url: https://api.github.com/repos/${REPO_SLUG}/releases"
 
     # Create the release in GitHub and extract its id from the response
     RESPONSE_BODY=$(curl -s \
@@ -25,10 +29,13 @@ uploadReleaseToGitHub() {
     # Extract the upload_url value
     UPLOAD_URL=$(echo ${RESPONSE_BODY} | jq -r .upload_url)
 
+    echo "Upload url: $UPLOAD_URL"
+
     cp annotations/build/libs/annotations.jar .
 
     # Attach annotations
     ANNOTATIONS_UPLOAD_URL=$(echo ${UPLOAD_URL} | sed "s/{?name,label}/?name=annotations-${THIS_TAG}.jar/")
+    echo "Annotations upload url: ${ANNOTATIONS_UPLOAD_URL}"
     curl -s \
         -v \
         -u ${CIRCLE_USERNAME}:${GITHUB_TOKEN} \
@@ -42,6 +49,7 @@ uploadReleaseToGitHub() {
 
     # Attach processor-java
     PROCESSOR_JAVA_UPLOAD_URL=$(echo ${UPLOAD_URL} | sed "s/{?name,label}/?name=processor-java-${THIS_TAG}.jar/")
+    echo "Processor java upload url: ${PROCESSOR_JAVA_UPLOAD_URL}"
     curl -s \
         -v \
         -u ${CIRCLE_USERNAME}:${GITHUB_TOKEN} \
